98303e73b6415311035117b05bf01a40df36491e
武斌斌 <50254643+shuangyuwbb@users.noreply.github.com>
2019-11-27 12:44:37 +0800

    Update index.js

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,24 +4,14 @@ Page({
 
     
     data: {
-        userinfo:{}
+       
     },
     //页面创建时执行
     onLoad: function(options) {
         
         
     },
-    //打印用户昵称
-    dayin:function(){
-        var that = this;
-        if(app.globalData.obj){
-        // that.setData({
-        //     userinfo:app.globalData.obj, 
-           
-        // })
-      console.log(app.globalData.obj)
-    }  
-},
+
     // 页面首次渲染完毕时执行
     onReady: function() {
         //Do some when page ready.
